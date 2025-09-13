f916c6a2b326f53e73fd4321e42c17cdf40eb645
FaseehFS <faseeh1080@gmail.com>
2025-09-13 21:30:02 +0530

    Add config command

--- repo_backup/main.py
+++ repo_backup/main.py
@@ -1,12 +1,24 @@
 import click
+import json
+
+
+CONFIG_FILE = "config.json"
+DEAFAULT_CONFIG = {"backup_directory": "cwd", "lfs": False, "pat": ""}
+
 
 @click.group()
 def cli():
-    """Repository manager CLI"""
     pass
 
+
 @cli.command()
-@click.argument('arg')
+def config():
+    with open(CONFIG_FILE, "w", encoding="utf-8") as f:
+        json.dump(DEAFAULT_CONFIG, f, indent=4)
+
+
+@cli.command()
+@click.argument("arg")
 def backup(arg):
     if arg == "all":
         click.echo("Not currently supported.")
@@ -17,9 +29,11 @@ def backup(arg):
     else:
         click.echo(f"backing up {arg}")
 
+
 @cli.command()
 def status():
     click.echo("Not currently supported.")
+
 
 if __name__ == "__main__":
     cli()
